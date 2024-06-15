a6af6f466800b0d91d719f3f148564bd7f970166
Thiago Christopher <thiago_cfc@hotmail.com>
2024-06-15 17:17:19 -0300

    feat: Preventing multiple instances 3

--- ScreenSleeper/Shared/Application.cs
+++ ScreenSleeper/Shared/Application.cs
@@ -6,12 +6,7 @@ namespace Shared
     public static class Application
     {
         private static readonly bool _isFirst;
-
+        private static readonly Mutex _mutex = new(true, name: "{04CF3278-CC61-4716-8B67-955C0FD7C469}", createdNew: out _isFirst);
         public static bool IsFirst => _isFirst;
-
-        static Application()
-        {
-            _ = new Mutex(true, name: "{04CF3278-CC61-4716-8B67-955C0FD7C469}", createdNew: out _isFirst);
-        }
     }
 }
