4315ca125fd37afc374fc5dab0deda650a2970cb
Stefan Tucker <stefan.tucker@outlook.com>
2024-06-15 22:49:44 -0500

    Publish 1.3.1. #patch

--- ScreenSleeper/Shared/Application.cs
+++ ScreenSleeper/Shared/Application.cs
@@ -6,6 +6,7 @@ namespace Shared
     public static class Application
     {
         private static readonly bool _isFirst;
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "Need to store to prevent future instances from creating mutex.")]
         private static readonly Mutex _mutex = new(true, name: "{04CF3278-CC61-4716-8B67-955C0FD7C469}", createdNew: out _isFirst);
         public static bool IsFirst => _isFirst;
     }
